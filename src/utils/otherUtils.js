d6d9effc27e5ec5766371133a08c9d3eb5ca688a
tsrrodrigues <tsrrodrigues@outlook.com>
2021-03-18 00:23:15 -0300

    Cria primeira versão do algoritmo ambicioso dos horários

--- src/utils/otherUtils.js
+++ src/utils/otherUtils.js
@@ -11,3 +11,15 @@ export const getStationsList = () => {
       }
     })
 }
+
+const getTime = (totalMinutes) => {
+  let hours = Math.floor(totalMinutes / 60)
+  let minutes = totalMinutes % 60
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  return `${hours}:${minutes}`
+}
